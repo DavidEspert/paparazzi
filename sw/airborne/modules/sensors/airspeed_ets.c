23e6700d757e3b3d6b9d6ad07e9149fc82dfc4f3
Gautier Hattenberger <gautier.hattenberger@enac.fr>
2016-03-17 18:32:26 +0100

    fix sdlog includes

--- sw/airborne/modules/sensors/airspeed_ets.c
+++ sw/airborne/modules/sensors/airspeed_ets.c
@@ -84,8 +84,7 @@ PRINT_CONFIG_VAR(AIRSPEED_ETS_I2C_DEV)
 PRINT_CONFIG_VAR(AIRSPEED_ETS_START_DELAY)
 
 #if AIRSPEED_ETS_SDLOG
-#include "sdLog.h"
-#include "subsystems/chibios-libopencm3/chibios_sdlog.h"
+#include "modules/loggers/sdlog_chibios.h"
 #include "subsystems/gps.h"
 bool_t log_airspeed_ets_started;
 #endif
