a7e1784c75b1c26933902513342e9dc624a751f8
Gautier Hattenberger <gautier.hattenberger@enac.fr>
2017-12-16 15:04:53 +0100

    update for new message name

--- sw/airborne/modules/fc_rotor/fc_rotor.c
+++ sw/airborne/modules/fc_rotor/fc_rotor.c
@@ -25,10 +25,6 @@
 #include "autopilot.h"
 #include "modules/fc_rotor/fc_rotor.h"
 
-#ifndef ABI_C
-#define ABI_C 1
-#endif
-
 int fc_rotor_on;
 
 void fc_rotor_init(void)
@@ -37,24 +33,24 @@ void fc_rotor_init(void)
 
 void fc_read_msg(void)
 {
-    struct FloatVect3 u;
-    uint8_t ac_id = DL_DESIRED_SET_POINT_ac_id(dl_buffer);
+  struct FloatVect3 u;
+  uint8_t ac_id = DL_DESIRED_SETPOINT_ac_id(dl_buffer);
 
-    if (ac_id == AC_ID) {
-      uint8_t dim = DL_DESIRED_SET_POINT_flag(dl_buffer);
+  if (ac_id == AC_ID) {
+    uint8_t flag = DL_DESIRED_SETPOINT_flag(dl_buffer);
 
-      u.x = DL_DESIRED_SET_POINT_ux(dl_buffer);
-      u.y = DL_DESIRED_SET_POINT_uy(dl_buffer);
-      u.z = DL_DESIRED_SET_POINT_uz(dl_buffer);
+    u.x = DL_DESIRED_SETPOINT_ux(dl_buffer);
+    u.y = DL_DESIRED_SETPOINT_uy(dl_buffer);
+    u.z = DL_DESIRED_SETPOINT_uz(dl_buffer);
 
-      if(dim == 1)
-      {
-        AbiSendMsgACCEL_SP(ACCEL_SP_3D_ID, &u);
-      }
-      else if(dim == 0)
-      {
-        AbiSendMsgACCEL_SP(ACCEL_SP_2D_ID, &u);
-      }
+    if (flag == 1) {
+      AbiSendMsgACCEL_SP(ACCEL_SP_3D_ID, &u);
     }
+    else if (flag == 0) {
+      AbiSendMsgACCEL_SP(ACCEL_SP_2D_ID, &u);
+
+      // TODO set altitude from u.z
+    }
+  }
 }
 
